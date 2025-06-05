9d60ece9ae18c74d7d6e5922bded6ceffa1c45bc
Vijaykumar1010 <123072467+Vijaykumar1010@users.noreply.github.com>
2025-06-05 17:13:01 +0530

    Add files via upload

---
+++ Funtion getSum to getSum of two Number.py
@@ -0,0 +1,31 @@
+# Funtion getSum to getSum of two Number
+
+def getSum_of_two_number( number1:int, number2:int)-> int:
+
+    # Adding the two number
+
+    Add_two_number = number1 + number2
+
+    return Add_two_number
+
+# invocation the function
+
+number1 = 10
+
+number2 = 20
+
+# Call the function and store the result
+
+result = getSum_of_two_number(number1, number2)
+
+print(result)
+
+# or
+def getsum(number3, number4):
+   return number3 + number4
+
+# invocation code
+
+sum = getsum(1, 20)
+
+print(sum)
